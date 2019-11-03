98990375c0bf293e06275960ae646c5be12398a2
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-11-02 21:46:03 -0300

    criando tabela de projetos

--- Projeto/frontend/frontend/src/components/admin/ProjectsAdmin.vue
+++ Projeto/frontend/frontend/src/components/admin/ProjectsAdmin.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="project-admin">
-        <h1>Artigo componente</h1>
+        <b-table hover striped :items="projects" :fields="fields">
+
+        </b-table>
     </div>
 </template>
 
 <script>
+// import { baseApiUrl } from '../../global'
+// import axios from 'axios'
+
 export default {
-    name: 'ProjectsAdmin'
+    name: 'ProjectsAdmin',
+    data(){
+        return{
+            mode: 'save',
+            project: {},
+            projects: [],
+            fields: [
+                { key: 'id', label: 'ID', sortable: true},
+                { key: 'Nome', label: 'Título', sortable: true},
+                { key: 'professor', label: 'Professor Responsável', sortable: true},
+                { key: 'acoes', label: 'Ações'}
+            ]
+        }
+    },
+
+    //essa parte serve pra buscar os projetos do backend
+
+    // methods:{
+    //     loadProjects(){ 
+    //         const url = `${ baseApiUrl }/projects`
+    //         axios.get(url).then(res=>{
+    //             this.projects = res.data
+    //         })
+    //     }
+    // },
+    // mount(){
+    //     this.loadProjects()
+    // }
 }
 </script>
 
