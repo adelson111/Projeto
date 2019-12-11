d4d5c5c29a36c052e01a7d109e40f5b25ff2f55d
matheuskaio <matheuskaio@github.com>
2019-12-10 23:00:24 -0300

    commit, antes de puxar

--- Projeto/frontend/frontend/src/components/admin/AdminPages.vue
+++ Projeto/frontend/frontend/src/components/admin/AdminPages.vue
@@ -1,30 +1,6 @@
 <template>
-<<<<<<< HEAD
-    <div class="admin-pages">
-        <PageTitle 
-            icon="fa fa-cogs"
-            main="Administração do Sistema"
-            sub="Cadastro de Projetos"
-        />
-        <div class="admin-pages-tabs">
-            <b-card no-body>
-                <b-tabs card>
-                    <b-tab title="Cadastrar Projeto" active>
-                        <AddProject/>
-                    </b-tab>
-                    <b-tab title="Projetos">
-                        <CategoryAdmin/>
-                    </b-tab>
-                </b-tabs>
-            </b-card>
-        </div>
-=======
   <div class="admin-pages">
-    <PageTitle
-      icon="fa fa-cogs"
-      main="Administração do Sistema"
-      sub="Cadastro de Projetos"
-    />
+    <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastro de Projetos" />
     <div class="admin-pages-tabs">
       <b-card no-body>
         <b-tabs card>
@@ -39,36 +15,18 @@
           </b-tab>
         </b-tabs>
       </b-card>
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
     </div>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import PageTitle from '../template/PageTitle'
-import CategoryAdmin from './CategoryAdmin'
-import AddProject from './AddProject'
-
-export default {
-    name: 'AdminPages',
-    components: { PageTitle, CategoryAdmin, AddProject }
-}
-</script>
-
-<style>
-    .card-header ul li a{
-        color: black
-    }
-</style>
-=======
 import PageTitle from "../template/PageTitle";
 import CategoryAdmin from "./CategoryAdmin";
 import AddProject from "./AddProject";
-import ToolsAdmin from "./ToolsAdmin";
+// import ToolsAdmin from "./ToolsAdmin";
 export default {
   name: "AdminPages",
-  components: { PageTitle, CategoryAdmin, AddProject, ToolsAdmin }
+  components: { PageTitle, CategoryAdmin, AddProject }
 };
 </script>
 
@@ -77,4 +35,3 @@ export default {
   color: #000;
 }
 </style>
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
--- Projeto/frontend/frontend/src/components/template/Menu.vue
+++ Projeto/frontend/frontend/src/components/template/Menu.vue
@@ -1,172 +1,99 @@
 <template>
-<<<<<<< HEAD
-    
-        <aside class="menu" v-show="isMenuVisible">
-            <div class="menu-filter">
-                <i class="fa fa-search fa-lg"></i>
-                <input type="text" placeholder="Buscar projeto" 
-                    v-model="treeFilter" class="filter-field"
-                >
-            </div>   
-             <div role="tree" class="tree">
-            <ul class="tree-root">
-                <router-link to="/meus-projetos-de-pesquisa" class="links">
-                    <li data-id="7adb27d4-380a-c909-d43e-330d8d61cdc3" class="tree-node  draggable">
-                    <div class="tree-content" style="padding-left: 0px;">
-                        <i class="tree-arrow ltr">
-                        </i> <!----> <span tabindex="-1" class="tree-anchor">
-                            <span>Pesquisa</span></span>
-                    </div> <!---->
-                </li>
-                </router-link>
-                <router-link to="/meus-projetos-de-extensao"  class="links">
-                    <li data-id="cb78b744-af39-1d0a-f382-8270114363ce" class="tree-node draggable">
-                    <div class="tree-content" style="padding-left: 0px;"><i class="tree-arrow ltr">
-                        </i> <!----> <span tabindex="-1" class="tree-anchor"><span>Extensão</span>
-                        </span></div> <!----></li>
-                </router-link>
-                <router-link to="/meus-tccs"  class="links">
-                     <li data-id="f730f105-309c-87a2-5bd9-1b99e1edd242" class="tree-node draggable">
-                    <div class="tree-content" style="padding-left: 0px;"><i class="tree-arrow ltr">
-                        </i> <!----> <span tabindex="-1" class="tree-anchor"><span>TCC</span>
-                        </span>
-                        </div> <!---->
-                </li>
-                </router-link>
-            </ul> <!---->
-        </div>   
-        </aside>
-       
-    
-=======
-    <div class="menu">
-        <aside class="menu-aside" v-show="isMenuVisible">
-        <div class="menu-filter">
-            <i class="fa fa-search fa-lg"></i>
-            <input type="text" placeholder="Buscar projeto" 
-                v-model="treeFilter" class="filter-field"
-            >
-        </div>
-        <Tree :data="treeData"  :filter="treeFilter" ref="tree" />
+  <div class="menu">
+    <aside class="menu-aside" v-show="isMenuVisible">
+      <div class="menu-filter">
+        <i class="fa fa-search fa-lg"></i>
+        <input type="text" placeholder="Buscar projeto" v-model="treeFilter" class="filter-field" />
+      </div>
+      <Tree :data="treeData" :filter="treeFilter" ref="tree" />
     </aside>
     <div class="verticalMenu">
-        <router-link to="/home-admin">Pesquisa </router-link>    
-        <router-link to="/home-admin">Extenção  </router-link>     
-        <router-link to="/home-admin">TCC's  </router-link>     
+      <router-link to="/home-admin">Pesquisa</router-link>
+      <router-link to="/home-admin">Extenção</router-link>
+      <router-link to="/home-admin">TCC's</router-link>
     </div>
-    </div>
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
+  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-<<<<<<< HEAD
-
-=======
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
+import { mapState } from "vuex";
 
 export default {
-    name: 'Menu',
-    computed: mapState(['isMenuVisible']),
-<<<<<<< HEAD
-=======
-
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
-    data(){
-        return{
-            treeFilter: '',
-            filter:{
-                emptyText: 'Projeto não encontrado!'
-            }
-        }
-    }
-}
+  name: "Menu",
+  computed: mapState(["isMenuVisible"]),
+  data() {
+    return {
+      treeFilter: "",
+      filter: {
+        emptyText: "Projeto não encontrado!"
+      }
+    };
+  }
+};
 </script>
 
-<style>  
+<style>
+.links:hover {
+  text-decoration: none;
+}
+.menu {
+  grid-area: menu;
+  background-color: #7cd17f;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
 
-    .links:hover{
-        text-decoration: none;
-    }
-    .menu{
-        grid-area: menu;
-<<<<<<< HEAD
-        background-color:#7CD17F;
-=======
-        background-color:#7CD17F;        
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-    }
-    
-    .verticalMenu{
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-start;
-        padding: 12px;
-    }
-    .verticalMenu a:hover{
-        background-color: #2F4F4F;
-        color: #fff;
-        text-decoration: none;
-    }
-<<<<<<< HEAD
+.verticalMenu {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 12px;
+}
+.verticalMenu a:hover {
+  background-color: #2f4f4f;
+  color: #fff;
+  text-decoration: none;
+}
+.verticalMenu a {
+  color: #000;
+  font-size: 1.3rem;
+  font-weight: 300;
+  text-decoration: none;
+}
+.menu .menu-filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    .tree{
-        padding: 12px;
-        font-size: 1.3rem;
-    }
+  margin: 20px;
+  margin-bottom: 8px;
+  border-bottom: 1px solid #8fbc8f;
+}
 
-    .menu .tree-node.selected > .tree-content,
-    .menu .tree-node .tree-content:hover
-    {
-        background-color: rgba(255, 255, 255, 0.2)
-    }
+.menu .menu-filter i {
+  color: #2f4f4f;
+  margin-right: 10px;
+}
 
-    .tree-arrow.has-child{
-        filter: brightness(2);
-=======
-    .verticalMenu a{
-        color: #000;
-        font-size: 1.3rem;
-        font-weight: 300;
-        text-decoration: none;
->>>>>>> 3c4c7461032f088f2115f89af792c83278abc54f
-    }
-    .menu .menu-filter{
-        display: flex;
-        justify-content: center;
-        align-items: center;
+.menu input {
+  color: #2f4f4f;
+  font-size: 1.3rem;
+  border: 0;
+  outline: 0;
+  width: 100%;
+  background: transparent;
+}
 
-        margin:  20px;
-        margin-bottom: 8px;
-        border-bottom: 1px solid #8FBC8F 
-    }
+.tree-filter-empty {
+  color: #fff;
+  margin-left: 20px;
+  font-size: 1.3rem;
+}
 
-    .menu .menu-filter i{
-        color: #2F4F4F;
-        margin-right: 10px;
-    }
-
-    .menu input{
-        color:  #2F4F4F;
-        font-size: 1.3rem;
-        border: 0;
-        outline: 0;
-        width: 100%;
-        background: transparent;
-    }
-
-    .tree-filter-empty{
-        color: #fff;
-        margin-left: 20px;
-        font-size: 1.3rem
-    }
-
-    .tree-root .router-link-exact-active:hover{
-        text-decoration: none;
-    }
+.tree-root .router-link-exact-active:hover {
+  text-decoration: none;
+}
 </style>
